b4d417a59d8a22c92cfb239cd7701a0b1eef2a12
Danila Salovskiy <danilabar@mail.ru>
2022-12-27 01:47:44 +0300

    add resolution 15.2

---
+++ 15-clokub-2-lb/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "yandex_cloud_id" {
+  default = "b1g62d8ululsummdnj71"
+}
+
+variable "yandex_folder_id" {
+  default = "b1goaocmukt0m7s34gke"
+}
+
+variable "a-zone" {
+  default = "ru-central1-a"
+}
+
+variable "lamp-instance-image-id" {
+  default = "fd827b91d99psvq5fjit"
+}
